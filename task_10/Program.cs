de11ce3022eb4added030f260e8fa39b1df2088d
Alex Radomsky <radomsky2910@gmail.com>
2022-12-19 13:54:21 +0300

    task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+Console.Write("Enter a 3-digit number: "); 
+try {
+  int number = Convert.ToInt32(Console.ReadLine()); 
+  int Length = number.ToString().Length; 
+  if (Length == 3) { 
+    int result = ( number/10 ) % 10; 
+    Console.WriteLine($"The second digit is {result}"); 
+  } else { 
+    Console.WriteLine("Not a 3-digit number"); 
+  }
+} catch (Exception e) {
+	Console.WriteLine(e.Message);
+}
